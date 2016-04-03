b014f3826bbb3a5dffd9839238a93a38c5d972f3
Andrew Moffat <andrew.robert.moffat@gmail.com>
2016-04-03 02:59:07 -0500

    rename file and remove scipy dep

--- __init__.py
+++ __init__.py
@@ -2,11 +2,17 @@ from math import sin, cos, ceil, floor, pi
 import importlib
 import bpy
 import mathutils
+from os.path import join, exists
+from os import mkdir
+from mathutils import Vector, Quaternion, Euler
 from contextlib import contextmanager
 from uuid import uuid4
 from bpy.utils import register_module, unregister_module
 from bpy import props as p
 import json
+import inspect
+import sys
+from pyhull.delaunay import DelaunayTri as Delaunay
 
 
 
@@ -25,6 +31,16 @@ game engine.",
 
 JSON_FILE_NAME = "lightprobes.json"
 FAILSAFE_OFFSET = 0.00001
+BAKE_SIZE = 32
+
+CUBEMAP_DIRECTION_LOOKUP = {
+    "posx": Quaternion((0.5, 0.5, -0.5, -0.5)),
+    "negx": Quaternion((0.5, 0.5, 0.5, 0.5)),
+    "posy": Quaternion((0, 0.0, -1.0, 0.0)),
+    "negy": Quaternion((0.0, 0.0, 0.0, -1.0)),
+    "posz": Quaternion((0, 0, -0.7071067690849304, -0.70710688829422)),
+    "negz": Quaternion((-0.70710688829422, -0.7071067690849304, 0, 0.0)),
+}
 
 # http://cseweb.ucsd.edu/~ravir/papers/envmap/envmap.pdf
 spherical_harmonics = {
@@ -46,13 +62,20 @@ spherical_harmonics = {
 def is_lightprobe(ob):
     return ob.name.startswith("lightprobe-")
 
+def is_cubemap(ob):
+    return ob.name.startswith("cubemap_probe-")
+
 def all_active_lightprobes():
     for ob in bpy.context.scene.objects:
         if is_lightprobe(ob):
             yield ob
 
 def get_all_lightprobe_data():
-    all_data = []
+    probe_data = []
+    all_data = {
+        "probes": probe_data,
+    }
+
     scale_by = bpy.context.scene.unit_settings.scale_length
     
     for probe in all_active_lightprobes():
@@ -64,10 +87,99 @@ def get_all_lightprobe_data():
         data["loc"] = [p*scale_by for p in list(probe.location)]
         data["name"] = probe.lightprobe.name or None
         data["coeffs"] = coeffs
-        all_data.append(data)
-        
+
+        probe_data.append(data)
+
+    point_data = [d["loc"] for d in probe_data]
+    tris = Delaunay(point_data)
+    simplices = tris.vertices
+    all_data["simplices"] = simplices
+
+
+    # since we're using qhull now, and no longer scipy, we must construct our
+    # neighbor structure manually
+    neighbors = []
+    for simp_idx, simp in enumerate(simplices):
+        cur_neighbors = []
+        neighbors.append(cur_neighbors)
+
+        for vert_idx_idx, vert_idx in enumerate(simp):
+
+            # construct a set of vert indices that we want to search the other
+            # simplices for
+            to_match = list(simp)
+            to_match.pop(vert_idx_idx)
+            to_match = set(to_match)
+
+            neighbor = None
+
+            for search_simp_idx, search_simp in enumerate(simplices):
+                # skip the simplex that we're doing a search on
+                if simp_idx == search_simp_idx:
+                    continue
+
+                try_match = set(search_simp)
+                if to_match < try_match:
+                    neighbor = search_simp_idx
+                    break
+
+            cur_neighbors.append(neighbor)
+
+    all_data["neighbors"] = neighbors
     return all_data
+
+
+@contextmanager
+def values(values):
+    restore_fns = []
     
+    def create_restore(ob, name, value):
+        def restore():
+            setattr(ob, name, value)
+        return restore
+    
+    for ob, ob_values in values.items():
+        for name, value in ob_values.items():
+            old_value = getattr(ob, name)
+            setattr(ob, name, value)
+            restore_fns.append(create_restore(ob, name, old_value))
+            
+    try:
+        yield
+    finally:
+        for fn in restore_fns:
+            try:
+                fn()
+            except:
+                pass
+            
+
+
+def render_cubemap(scene, ob, size, get_filename, progress_update=None):
+    bpy.ops.object.camera_add()
+    cam = bpy.context.active_object
+    
+    cam.data.lens_unit = "FOV"
+    cam.data.angle = pi/2
+    cam.location = ob.location
+    cam.rotation_mode = "QUATERNION"
+
+    def render(direction):
+        cam.rotation_quaternion = CUBEMAP_DIRECTION_LOOKUP[direction]
+        filepath = get_filename(direction)
+        with values({scene.render: {"resolution_x": size, "resolution_y": size,
+                "filepath": filepath}, scene: {"camera": cam},
+                scene.render.image_settings: {"file_format": "OPEN_EXR"},
+                ob: {"hide": True}}):
+            bpy.ops.render.render(animation=False, write_still=True)
+        
+    for direction in CUBEMAP_DIRECTION_LOOKUP.keys():
+        if progress_update:
+            progress_update()
+        render(direction)
+        
+    bpy.ops.object.delete()
+        
 
 def get_or_create_probe_file():
     if JSON_FILE_NAME not in bpy.data.texts:
@@ -80,7 +192,6 @@ def get_or_create_probe_file():
 def write_lightprobe_data(data):
     f = get_or_create_probe_file()
     f.clear()        
-    
     data = json.dumps(data, indent=4, sort_keys=True)
     f.write(data)
     
@@ -114,12 +225,15 @@ def post_bake_hook(name, context, data, pre_bake_data):
 
 def hide_object(ob):
     """ hides an object from cycles rendering, and returns a function that,
-    when called, will restore the visibility """ 
-    v = ob.cycles_visibility
-
-    names = ("shadow", "camera", "glossy", "diffuse", "transmission", "scatter")
-    for name in names:
-        setattr(v, name, False)
+    when called, will restore the visibility """
+    print("hiding object", ob) 
+    old_value = ob.hide_render
+    
+    ob.hide_render = True    
+    def restore():
+        ob.hide_render = old_value
+            
+    return restore
         
         
 def set_coeff_prop(ob, coeffs):
@@ -144,7 +258,7 @@ def get_coeff_prop(ob):
     
 
 
-def setup_material(ob):
+def setup_lightprobe_material(ob):
     scene = bpy.context.scene
     
     mat = bpy.data.materials.new(ob.name)
@@ -155,7 +269,6 @@ def setup_material(ob):
     for node in list(tree.nodes):
         tree.nodes.remove(node)
         
-    #diffuse = tree.nodes.new("ShaderNodeBsdfGlossy")
     diffuse = tree.nodes.new("ShaderNodeBsdfDiffuse")
     diffuse.inputs["Color"].default_value = (1, 1, 1, 1)
     diffuse.inputs["Roughness"].default_value = 1.0
@@ -170,12 +283,10 @@ def setup_material(ob):
     bake_node.label = bake_node.name
     bake_out = bake_node.outputs["Color"]
     
-    texture = create_lightmap_image(ob, 128, 128)
+    texture = create_lightmap_image(ob, BAKE_SIZE, BAKE_SIZE)
     bake_node.image = texture
     
-    
     ob.data.uv_textures["lightmap"].active = True
-    
     
     color_uvmap = tree.nodes.new("ShaderNodeUVMap")
     color_uvmap.uv_map = "lightmap"
@@ -186,17 +297,18 @@ def setup_material(ob):
 
 
 
-
 @contextmanager
 def no_interfere_ctx():
     old_selected_objects = bpy.context.selected_objects
     active_object = bpy.context.active_object
-    yield
-    for obj in bpy.context.selected_objects:
-        obj.select = False
-    for obj in old_selected_objects:
-        obj.select = True
-    bpy.context.scene.objects.active = active_object
+    try:
+        yield
+    finally:
+        for obj in bpy.context.selected_objects:
+            obj.select = False
+        for obj in old_selected_objects:
+            obj.select = True
+        bpy.context.scene.objects.active = active_object
     
     
 @contextmanager
@@ -206,6 +318,13 @@ def active(ob):
         bpy.context.scene.objects.active = ob
         yield
     
+def hide_all(scene):
+    restores = {}
+    for ob in scene.objects:
+        restore = hide_object(ob)
+        restores[ob] = restore
+    return restores
+
 
 def override_ctx(**kwargs):
     ctx = bpy.context.copy()
@@ -216,7 +335,7 @@ def override_ctx(**kwargs):
 def create_lightmap_image(ob, width, height):
     name = ob.name
     bpy.ops.image.new(override_ctx(object=ob), name=name, width=width,
-            height=height, alpha=False, float=False)
+            height=height, alpha=False, float=True)
     return bpy.data.images[name]
 
 def get_lightmap(ob):
@@ -253,12 +372,38 @@ def add_lightprobe():
     probe.name = "lightprobe-" + uuid4().hex
     return probe
 
+
+def add_cubemap_probe():
+    with no_interfere_ctx():
+        bpy.ops.mesh.primitive_cube_add()
+        probe = bpy.context.object
+        
+        for _ in range(len(probe.data.uv_layers)):
+            bpy.ops.mesh.uv_texture_remove()
+        
+        bpy.ops.mesh.uv_texture_add()
+        probe.data.uv_layers[0].name = "lightmap"
+        
+        bpy.ops.uv.lightmap_pack(PREF_CONTEXT="ALL_FACES",
+                PREF_PACK_IN_ONE=True, PREF_NEW_UVLAYER=False,
+                PREF_APPLY_IMAGE=False, PREF_IMG_PX_SIZE=512, PREF_BOX_DIV=12,
+                PREF_MARGIN_DIV=0.1)
+        
+        probe.scale = mathutils.Vector((0.3, 0.3, 0.3))
+        
+    probe.name = "cubemap_probe-" + uuid4().hex
+    hide_object(probe)
+    return probe    
+
+
 def bake(ob):
-    with active(ob):        
-        cycles = bpy.context.scene.cycles
+    with active(ob):
+        scene = bpy.context.scene
+
+        cycles = scene.cycles
         old_samples = cycles.samples
         
-        cycles.samples = ob.lightprobe.samples        
+        cycles.samples = scene.lightprobe.samples        
         
         bpy.ops.object.bake(type="COMBINED")
         cycles.samples = old_samples
@@ -533,9 +678,37 @@ class LightProbeConfigPanel(bpy.types.Panel):
         row.alert = bool(name and fn is None)
         row.prop(scene.lightprobe, "post_bake_hook")
         
-        layout.operator(BakeAllOperator.bl_idname)
+        layout.prop(scene.lightprobe, "cubemap_dir")
         
+        row = layout.row()
+        row.prop(scene.lightprobe, "theta_res")
+        row.prop(scene.lightprobe, "phi_res")
+        
+        layout.prop(scene.lightprobe, "samples")
+
+        layout.operator(BakeAllOperator.bl_idname)
         layout.operator(ResizeAllOperator.bl_idname)
+    
+    
+    
+class CubemapProbePanel(bpy.types.Panel):
+    bl_label = "Cubemap Probe"
+    bl_space_type = "PROPERTIES"
+    bl_region_type = "WINDOW"
+    bl_context = "object"
+    
+    @classmethod
+    def poll(self, context):
+        return is_cubemap(context.object)
+    
+    def draw(self, context):
+        ob = context.object
+        layout = self.layout
+        
+        layout.prop(ob.cubemap, "name")
+        layout.prop(ob.cubemap, "sky_only")
+        layout.prop(ob.cubemap, "size")
+        layout.operator(BakeCubemapOperator.bl_idname)
     
     
 class LightProbePanel(bpy.types.Panel):
@@ -558,36 +731,76 @@ class LightProbePanel(bpy.types.Panel):
         row = layout.row()
         row.prop(lp, "name")
         
-        row = layout.row()
-        row.prop(lp, "theta_res")
-        row.prop(lp, "phi_res")
-        
-        layout.prop(lp, "samples")
-        
-        row = layout.row()
-        row.enabled = context.scene.render.engine == "CYCLES"
-        row.operator(BakeOperator.bl_idname)
+        layout.operator(BakeOperator.bl_idname)
 
+
+class BakeCubemapOperator(bpy.types.Operator):
+    bl_idname = "object.bake_cubemap"
+    bl_label = "Bake Cube Map"
+    
+    @classmethod
+    def poll(cls, context):
+        cycles = context.scene.render.engine == "CYCLES"
+        has_name = context.object.cubemap.name
+        has_dir = context.scene.lightprobe.cubemap_dir
+        return cycles and has_name and has_dir
+    
+    def execute(self, context):
+        probe = context.object
+        scene = context.scene
+        size = probe.cubemap.size
+        
+        dir = bpy.path.abspath(join(scene.lightprobe.cubemap_dir,
+                probe.cubemap.name))
+        if not exists(dir):
+            mkdir(dir)        
+        
+        def get_filename(direction):
+            name = "%s.exr" % direction
+            path = join(dir, name)
+            return path
+        
+        def update_gen():
+            wm = context.window_manager
+            wm.progress_begin(0, 5)
+            
+            for i in range(6):
+                wm.progress_update(i)
+                yield
+        awesome = update_gen()
+        update_fn = lambda: next(awesome)
+        
+        sky_only = probe.cubemap.sky_only
+        with no_interfere_ctx():
+            if sky_only:
+                restores = hide_all(scene)
+            restores[probe]()
+            try:
+                render_cubemap(context.scene, probe, size, get_filename, update_fn)
+            finally:
+                for restore in restores.values():
+                    restore()
+                    
+        return {"FINISHED"}
+    
         
 class BakeOperator(bpy.types.Operator):
     bl_idname = "object.bake_lightprobe"
     bl_label = "Bake Light Probe"
+    
+    @classmethod
+    def poll(cls, context):
+        cycles = context.scene.render.engine == "CYCLES"
+        return cycles
 
     def execute(self, context):
         probe = context.object
+        scene = context.scene
         
-        scene_settings = context.scene.lightprobe
-        settings = probe.lightprobe
-        
-        ret = pre_bake_hook(scene_settings.pre_bake_hook, context, probe)
-        
+        settings = scene.lightprobe
         coeffs = get_lightprobe_coefficients(probe, settings.theta_res,
                 settings.phi_res)
         set_coeff_prop(probe, coeffs)
-        lp_data = get_all_lightprobe_data()
-        write_lightprobe_data(lp_data)
-        
-        post_bake_hook(scene_settings.post_bake_hook, context, lp_data, ret)
         
         return {"FINISHED"}
     
@@ -597,11 +810,21 @@ class BakeAllOperator(bpy.types.Operator):
     bl_label = "Bake All Light Probes"
 
     def execute(self, context):
+        scene_settings = context.scene.lightprobe
+
+        all_probes = all_active_lightprobes()
+        ret = pre_bake_hook(scene_settings.pre_bake_hook, context, all_probes)
+        
         override = context.copy()
-        for probe in all_active_lightprobes():
+        for probe in all_probes:
             override["object"] = probe
             bpy.ops.object.bake_lightprobe(override)
         
+        lp_data = get_all_lightprobe_data()
+        write_lightprobe_data(lp_data)
+        post_bake_hook(scene_settings.post_bake_hook, context, lp_data, ret)
+
+
         return {"FINISHED"}
     
     
@@ -634,8 +857,17 @@ class LightProbeOperator(bpy.types.Operator):
         probe.show_x_ray = True
         
         with active(probe):
-            setup_material(probe)
+            setup_lightprobe_material(probe)
             
+        return {"FINISHED"}
+    
+    
+class AddCubemapOperator(bpy.types.Operator):
+    bl_idname = "object.add_cubemap_probe"
+    bl_label = "Add Cubemap Probe"
+    
+    def execute(self, context):
+        probe = add_cubemap_probe()
         return {"FINISHED"}
 
 
@@ -643,16 +875,23 @@ class SceneProperties(bpy.types.PropertyGroup):
     pre_bake_hook = p.StringProperty(name="Pre-bake hook")
     post_bake_hook = p.StringProperty(name="Post-bake hook", description="""Call \
 this function with lightprobe data.  Used for integrating with other plugins.""")
+    cubemap_dir = p.StringProperty(name="Cubemap Directory", default="", subtype="DIR_PATH")
+    theta_res = p.IntProperty(name="Theta Samples", default=10)
+    phi_res = p.IntProperty(name="Phi Samples", default=20)
+    samples = p.IntProperty(name="Bake samples", default=50)
     
 class ProbeProperties(bpy.types.PropertyGroup):
     name = p.StringProperty(name="Probe Name", default="")
-    theta_res = p.IntProperty(name="Theta Samples", default=10)
-    phi_res = p.IntProperty(name="Phi Samples", default=20)
-    samples = p.IntProperty(name="Bake samples", default=1000)
+    
+class CubemapProperties(bpy.types.PropertyGroup):
+    name = p.StringProperty(name="Probe Name", default="")
+    size = p.IntProperty(name="Size", default=256)
+    sky_only = p.BoolProperty(name="Sky only", default=False)
     
     
 def register():
     register_module(__name__)
+    bpy.types.Object.cubemap = p.PointerProperty(type=CubemapProperties)
     bpy.types.Object.lightprobe = p.PointerProperty(type=ProbeProperties)
     bpy.types.Scene.lightprobe = p.PointerProperty(type=SceneProperties)
 
@@ -663,9 +902,9 @@ def unregister():
     del bpy.types.Object.lightprobe
     del bpy.types.Scene.lightprobe
     
-
 try:
     unregister()
 except:
     pass
 register()
+
